c7397d5b8c0b361ea4218a20069b6a9294adb681
Meyer Florian <meyerf93@gmail.com>
2016-12-14 00:06:21 +0100

    make new green_living  project

---
+++ GreenLiving/app/src/main/java/hes_so/greenliving/Green_Living.java
@@ -0,0 +1,13 @@
+package hes_so.greenliving;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Green_Living extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_green_living);
+    }
+}
