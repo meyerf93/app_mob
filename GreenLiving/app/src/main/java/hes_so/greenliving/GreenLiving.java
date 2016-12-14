a8c377351266a28d10b67261e8d77f6d44403061
Meyer FLorian <meyerf93@gmail.com>
2016-12-14 15:15:41 +0100

    create green livin main activity

--- GreenLiving/app/src/main/java/hes_so/greenliving/GreenLiving.java
+++ GreenLiving/app/src/main/java/hes_so/greenliving/GreenLiving.java
@@ -3,7 +3,7 @@ package hes_so.greenliving;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Green_Living extends AppCompatActivity {
+public class GreenLiving extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
