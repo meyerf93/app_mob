b33fb9d7dc9b37770cca6d6af3efd22d7340e4b0
Meyer FLorian <meyerf93@gmail.com>
2017-01-03 07:26:29 +0100

    beginnin of the resume part

---
+++ GreenLiving/app/src/main/java/hes_so/greenliving/Resume/CustomResume.java
@@ -0,0 +1,10 @@
+package hes_so.greenliving.Resume;
+
+/**
+ * Created by Florian.Meyer on 03.01.2017.
+ */
+
+public class CustomResume {
+
+
+}
---
+++ GreenLiving/app/src/main/java/hes_so/greenliving/Resume/ResumeAdapter.java
@@ -0,0 +1,8 @@
+package hes_so.greenliving.Resume;
+
+/**
+ * Created by Florian.Meyer on 03.01.2017.
+ */
+
+public class ResumeAdapter {
+}
